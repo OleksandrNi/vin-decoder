5f284d1694f5cce95330dc47d83d136f27cd4166
Alexandr Nikytenko <nika200382@gmail.com>
2022-07-11 19:14:29 +0300

    code refactoring

---
+++ src/components/TableComponent/TableDisplay.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './TableDisplay.scss'
+
+export const TableDisplay = ({ columns, results, columnWithLink }) => {
+
+  return (
+    <table className='table container'>
+        <thead>
+          <tr>
+            {columns.map(item => <th key={item}>{item}</th>)}
+          </tr>
+        </thead>
+        <tbody>
+          {results.map(item =>(
+            <tr key={item}>
+              {columns.map(column => {
+                if (column === columnWithLink) {
+                  return <th><Link to={`/variables/${item.ID}`}>{item[column]}</Link></th>
+                }
+                return <th>{item[column]}</th>
+              })}
+            </tr>
+          ))}
+        </tbody>
+    </table>
+  )
+};
